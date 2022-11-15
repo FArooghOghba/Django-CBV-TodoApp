f1c00c2d103276916a4e7cbcae9e507e3e069a46
FArooghOghba <faroogh.oghba@gmail.com>
2022-11-15 18:05:33 +0330

    build: custom user model
    create custom user with AbstractBaseUser model & change AUTH_USER_MODEL.

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
+
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, email, username, password, **extra_fields):
+        """
+        Create and save a User with the given email, username and password,
+        :param email: user email
+        :param username: username
+        :param password: user password
+        :param extra_fields: is_staff, is_superuser, is_active
+        :return: user
+        """
+
+        if not email:
+            raise ValueError(_('The Email must be set.'))
+        if not username:
+            raise ValueError(_('The Username must be set.'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, username, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password,
+        :param email: user email
+        :param username: username
+        :param password: user password
+        :param extra_fields: is_staff, is_superuser, is_active
+        :return: user - self.create_user(email, username, password)
+        """
+
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(email, username, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom User Model Based on AbstractBaseUser & PermissionMixin for
+    adding email as USERNAME FIELD.
+    """
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username',)
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
--- core/core/settings.py
+++ core/core/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# User Model Config
+
+AUTH_USER_MODEL = 'accounts.User'
