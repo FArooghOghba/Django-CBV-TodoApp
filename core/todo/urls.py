adcdafe8de5b1d6a1be6ce711957129ef761727d
FArooghOghba <faroogh.oghba@gmail.com>
2022-11-15 23:27:26 +0330

    build: create TaskCreateView
    create tasks with model form.

--- core/todo/urls.py
+++ core/todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView, TaskCreateView
 
 
-app_name = 'Task'
+app_name = 'task'
 
 
 urlpatterns = [
-    path('', TaskListView.as_view(), name='list')
+    path('list/', TaskListView.as_view(), name='list'),
+    path('create/', TaskCreateView.as_view(), name='create'),
 ]
--- core/todo/views.py
+++ core/todo/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
 from .models import Task
+from .forms import TaskForm
 
 
 # Create your views here.
@@ -9,3 +10,18 @@ from .models import Task
 class TaskListView(ListView):
     model = Task
     context_object_name = 'tasks'
+
+
+class TaskCreateView(CreateView):
+    """
+    View for creating a new Task object, with a response
+    rendered by a template.
+    """
+
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy('task:list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreateView, self).form_valid(form)
