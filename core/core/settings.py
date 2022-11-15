e06becd1ecb1dd0f38a33a0299f2520bd46a493c
FArooghOghba <faroogh.oghba@gmail.com>
2022-11-15 19:58:20 +0330

    build: task model & todo app

--- core/core/settings.py
+++ core/core/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'todo.apps.TodoConfig'
 ]
 
 MIDDLEWARE = [
---
+++ core/todo/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+# Get user model
+
+user = get_user_model()
+
+
+# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(user, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    descriptions = models.TextField()
+    complete = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['complete']
+
