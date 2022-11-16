0389cb770d44606ad90d7e8f12229ddb3282931c
FArooghOghba <faroogh.oghba@gmail.com>
2022-11-16 14:04:34 +0330

    build: create LoginView

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+
 
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    fields = ('email', 'password')
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('task:list')
--- core/core/urls.py
+++ core/core/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('task/', include('todo.urls'))
+    path('task/', include('todo.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
