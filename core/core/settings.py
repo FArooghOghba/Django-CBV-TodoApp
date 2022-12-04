e55b6584e353f3ec2a3cd86c0acf66b6a857907e
FArooghOghba <faroogh.oghba@gmail.com>
2022-12-04 21:15:08 +0330

    fix: add basic authentication

--- core/core/settings.py
+++ core/core/settings.py
@@ -161,6 +161,7 @@ if DEBUG:
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
