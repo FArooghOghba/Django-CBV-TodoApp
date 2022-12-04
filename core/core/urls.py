e5b1fead1061dc1c9fec154a0cf3fab2ac7e4c17
FArooghOghba <faroogh.oghba@gmail.com>
2022-12-04 21:00:12 +0330

    build: API document
    adding API document with drf-yasg for using swagger & redoc.

--- core/core/urls.py
+++ core/core/urls.py
@@ -16,11 +16,38 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from django.views.generic.base import RedirectView
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Todo App API",
+      default_version='v1',
+      description="Todo app for managing your tasks.",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="faroogh.oghba@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='task:list'), name='task-list-redirect'),
+
     path('admin/', admin.site.urls),
-    path('', include('todo.urls')),
+    path('task/', include('todo.urls')),
     path('accounts/', include('accounts.urls')),
 
     path('api-auth/', include('rest_framework.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
+
+    path('swagger/api.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- core/todo/urls.py
+++ core/todo/urls.py
@@ -8,11 +8,11 @@ app_name = 'task'
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='list'),
-    path('task/create/', TaskCreateView.as_view(), name='create'),
-    path('task/detail/<int:task_id>/', TaskDetailView.as_view(), name='detail'),
-    path('task/<int:task_id>/update/', TaskUpdateView.as_view(), name='update'),
-    path('task/<int:task_id>/complete/', TaskCompleteView.as_view(), name='complete'),
-    path('task/<int:task_id>/delete/', TaskDeleteView.as_view(), name='delete'),
+    path('create/', TaskCreateView.as_view(), name='create'),
+    path('detail/<int:task_id>/', TaskDetailView.as_view(), name='detail'),
+    path('<int:task_id>/update/', TaskUpdateView.as_view(), name='update'),
+    path('<int:task_id>/complete/', TaskCompleteView.as_view(), name='complete'),
+    path('<int:task_id>/delete/', TaskDeleteView.as_view(), name='delete'),
 
     path('api/v1/', include('todo.api.v1.urls'))
 ]
