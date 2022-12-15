a7e7230828a039ee575d3c1f57b43ad6f5302752
FArooghOghba <faroogh.oghba@gmail.com>
2022-12-15 19:03:06 +0330

    fix: registration
    moving create user method from view registration to serializer.

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -2,8 +2,12 @@ from rest_framework import serializers
 from rest_framework.status import HTTP_401_UNAUTHORIZED, HTTP_400_BAD_REQUEST
 
 from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.password_validation import validate_password
 from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
+
+
+User = get_user_model()
 
 
 class LoginSerializer(serializers.Serializer):
@@ -44,46 +48,54 @@ class LoginSerializer(serializers.Serializer):
         return attrs
 
 
-class RegisterSerializer(serializers.Serializer):
+class RegistrationModelSerializer(serializers.ModelSerializer):
     """
-    This serializer defines three fields for registration:
+    This serializer defines four fields for registration:
       * email
+      * username
       * password.
     It will try to register the user with, when validated.
     """
-    email = serializers.EmailField(max_length=128)
-    username = serializers.CharField(max_length=255)
-    password1 = serializers.CharField(
+    password = serializers.CharField(
+        max_length=255,
+        validators=[validate_password],
+        style={'input_type': 'password'},
+        write_only=True
+    )
+    password_confirm = serializers.CharField(
         max_length=255,
         style={'input_type': 'password'},
         write_only=True
     )
-    password2 = serializers.CharField(
-        max_length=255,
-        style={'input_type': 'password'},
-        write_only=True
-    )
+
+    class Meta:
+        model= User
+        fields = ('email', 'username', 'password', 'password_confirm')
 
     def validate(self, attrs):
         email = attrs.get('email')
         username = attrs.get('username')
-        password1 = attrs.get('password1')
-        password2 = attrs.get('password2')
+        password = attrs.get('password')
+        password_confirm = attrs.get('password_confirm')
 
-        if not password1 == password2:
+        if not password == password_confirm:
             raise serializers.ValidationError(
                 _('Passwords must be the same.'),
-                code=HTTP_401_UNAUTHORIZED
+                code=HTTP_400_BAD_REQUEST
             )
 
-        user = get_user_model().objects.filter(
+        user = User.objects.filter(
             Q(email=email) | Q(username=username)
         ).exists()
 
         if user:
             raise serializers.ValidationError(
                 _('User exists, login or choose another email.'),
-                code=HTTP_401_UNAUTHORIZED
+                code=HTTP_400_BAD_REQUEST
             )
 
         return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm', None)
+        return User.objects.create_user(**validated_data)
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,33 +1,33 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.status import HTTP_202_ACCEPTED, HTTP_401_UNAUTHORIZED
+from rest_framework.status import HTTP_201_CREATED, HTTP_202_ACCEPTED, HTTP_401_UNAUTHORIZED
 
 from django.contrib.auth import get_user_model, login, authenticate
 
-from .serializers import LoginSerializer, RegisterSerializer
+from .serializers import LoginSerializer, RegistrationModelSerializer
 
 
-class RegisterGenericAPIView(GenericAPIView):
+class RegistrationGenericAPIView(GenericAPIView):
     """
     Register view to get user credentials.
     This view should be accessible also for unauthenticated users.
     """
-    serializer_class = RegisterSerializer
+    serializer_class = RegistrationModelSerializer
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
+            serializer.save()
+
             email = serializer.validated_data['email']
             username = serializer.validated_data['username']
-            password = serializer.validated_data['password1']
+            password = serializer.validated_data['password']
 
-            user = get_user_model().objects.create_user(
-                email=email, username=username, password=password
-            )
-
-            authenticate(request, email=email, password=password)
-            login(request, user)
-            return Response(serializer.data, status=HTTP_202_ACCEPTED)
+            data = {
+                'email': email,
+                'username': username,
+            }
+            return Response(data, status=HTTP_201_CREATED)
 
         return Response(serializer.errors, status=HTTP_401_UNAUTHORIZED)
 
